e73aa8cd280b2054603e9bbcc44ec91afee0a98d
maxdlr <contact@maxdlr.com>
2024-09-23 22:04:16 +0200

    feat: mount postman, mockoon and frontend

--- ressources/sql/script.sql
+++ ressources/sql/script.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `USERS` (
+CREATE TABLE if not exists `USERS` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `email` varchar(255),
   `name` varchar(255),
@@ -7,7 +7,7 @@ CREATE TABLE `USERS` (
   `updated_at` timestamp
 );
 
-CREATE TABLE `RENTALS` (
+CREATE TABLE if not exists `RENTALS` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
   `surface` numeric,
@@ -19,7 +19,7 @@ CREATE TABLE `RENTALS` (
   `updated_at` timestamp
 );
 
-CREATE TABLE `MESSAGES` (
+CREATE TABLE if not exists `MESSAGES` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `rental_id` integer,
   `user_id` integer,
@@ -28,10 +28,13 @@ CREATE TABLE `MESSAGES` (
   `updated_at` timestamp
 );
 
-CREATE UNIQUE INDEX `USERS_index` ON `USERS` (`email`);
+CREATE UNIQUE INDEX IF NOT EXISTS `USERS_index` ON `USERS` (`email`);
 
-ALTER TABLE `USERS` ADD FOREIGN KEY (`id`) REFERENCES `RENTALS` (`owner_id`);
+ALTER TABLE `RENTALS` ADD CONSTRAINT `fk_owner_id` FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`);
+# ALTER TABLE `USERS` ADD FOREIGN KEY (`id`) REFERENCES `RENTALS` (`owner_id`);
 
-ALTER TABLE `USERS` ADD FOREIGN KEY (`id`) REFERENCES `MESSAGES` (`user_id`);
+ALTER TABLE `MESSAGES` ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
+# ALTER TABLE `USERS` ADD FOREIGN KEY (`id`) REFERENCES `MESSAGES` (`user_id`);
 
-ALTER TABLE `RENTALS` ADD FOREIGN KEY (`id`) REFERENCES `MESSAGES` (`rental_id`);
+ALTER TABLE `MESSAGES` ADD CONSTRAINT  `fk_rental_id` FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`);
+# ALTER TABLE `RENTALS` ADD FOREIGN KEY (`id`) REFERENCES `MESSAGES` (`rental_id`);
